7d4b3b9a47995abbd90a5333fab49a5fb6612f8a
Rishikesh Dhanavde <rishidhanavde@gmail.com>
2026-04-01 15:58:01 +0530

    design-code link
    linking the dice image in main.storyboard to ViewController.swift and changing image from code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //while in vm, use ctrl+alt to get IBOutlet from main.storyboard to ViewController
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //to get image literal, it is "#imageLiteral("
+        // who.what = value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
-
-
+    
 }
-
